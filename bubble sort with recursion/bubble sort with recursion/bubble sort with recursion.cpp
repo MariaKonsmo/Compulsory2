60fc261edcc97d8406f0054400bbc0e097e75b71
MariaKonsmo <111863862+MariaKonsmo@users.noreply.github.com>
2023-10-21 11:43:37 +0200

    bubble sort fixed

--- bubble sort with recursion/bubble sort with recursion/bubble sort with recursion.cpp
+++ bubble sort with recursion/bubble sort with recursion/bubble sort with recursion.cpp
@@ -10,31 +10,43 @@
 
 
 using namespace std;
-void bubbleSort(int arr[], int n)
-{
-    //lager swaperne i og j
-    int i;
-    int j;
-    bool swapped;
-    //lager to loops. flytter og bytter bortover til høyre, hvis tallet er lavere enn til høyre swapper tallene med swaperne i og j. hvis 
-    //høyere swaper ikke og fortsetter til de kommer til slutten der det høyeste tallet er, og så starter de på nytt til alt er sortert
-    //i riktig rekkefølge. når alt er sortert breaker loopen og den stopper.
-    for (i = 0; i < n - 1; i++) {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
-                swapped = true;
-            }
-        }
 
-        // If no two elements were swapped
-        // by inner loop, then break
-        if (swapped == false)
-            break;
-    }
+// Swap two elements in an array
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
+
+// Recursive Bubble Sort function
+void bubbleSort(int arr[], int n, int pass) {
+    // Base case: If no swaps are made, the array is already sorted
+    if (pass >= n - 1) {
+        return;
+    }
+
+    bool swapped = false;
+
+    // Perform a single pass of Bubble Sort
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            swap(arr[i], arr[i + 1]);
+            swapped = true;
+        }
+    }
+
+    // If no elements were swapped in this pass, the array is sorted.
+    if (!swapped) {
+        return;
+    }
+
+    // Recursively call the function for the next pass
+    bubbleSort(arr, n, pass + 1);
+}
+
+
 // Function to print an array
+
 void printArray(int arr[], int size)
 {
     int i;
@@ -47,8 +59,8 @@ long long measureselectionsorttimeinmilliseconds(int arr[], int n)
 {
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    //call quicksort function
-    bubbleSort(arr, n);
+    //call bubblesort function
+    bubbleSort(arr, n, 0);
 
     auto end_time = std::chrono::high_resolution_clock::now();
 
